6009b539c7a1250834636a7c68ee9edff6ccf9f3
w4ty <34249020+w4ty@users.noreply.github.com>
2025-05-28 14:12:09 +0200

    feat: animate slide in

--- script.js
+++ script.js
@@ -117,4 +117,29 @@ window.addEventListener('resize', () => {
             navMenu.classList.remove('active');
         }
     }
-}); 
+});
+
+const sectionsToAnimate = document.querySelectorAll('main section');
+
+if (sectionsToAnimate.length > 0) {
+    const sectionObserverOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const sectionObserverCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    const sectionObserver = new IntersectionObserver(sectionObserverCallback, sectionObserverOptions);
+
+    sectionsToAnimate.forEach(section => {
+        sectionObserver.observe(section);
+    });
+} 
